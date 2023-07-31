63cd65882113d8d74935b9493a1b52c7c28d866c
Thiago Falcao <thiago.falcao@alunos.sc.senac.br>
2023-07-31 20:45:36 -0300

    init

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Aula Web PHP</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Inicial</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+                <li><a href="contato.php">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Bem vindos ao meu site</h1>
+        <p>Seja bem vindo ao melhor site do mundo!</p>
+    </main>
+    <footer>
+        <p>Aqui é o rodapé</p>
+    </footer>
+</body>
+
+</html>
